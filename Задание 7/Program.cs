0c56bedd8ebe74dc12643c5c5ad07f20e61bfbdb
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2021-11-23 23:52:25 +0300

    Финализация

--- Задание 7/Program.cs
+++ Задание 7/Program.cs
@@ -10,6 +10,18 @@ namespace Задание_7
     {
         static void Main(string[] args)
         {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            PloshTr(a, b, c);
+            Console.ReadKey();
+        }
+        static void PloshTr(int a, int b, int c)
+        {
+            double p = (a + b + c) / 2;
+            double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine(S);
         }
     }
 }
+
